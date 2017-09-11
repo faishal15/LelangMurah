e7386fa124cb9651b528ebf141e40adec6490560
toscayconnery <tosca14@mhs.if.its.ac.id>
2017-09-11 15:36:09 +0700

    push dulu

---
+++ app/Http/Controllers/ItemController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ItemController extends Controller
+{
+    //This controller is useds to manage all the item
+
+    public function tambahkanItem(){
+    	//
+    }
+
+    public function hapusItem(){
+    	//
+    }
+
+    public function ubahStatusItem(){
+    	//
+    }
+}
---
+++ database/migrations/2017_09_11_081556_create_item_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateItemTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('item', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('nama');
+            $table->integer('id_kategori');
+            $table->integer('id_pemilik');
+            $table->string('kota');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('item');
+    }
+}
